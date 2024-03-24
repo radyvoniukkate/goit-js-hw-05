0623f45d513aa9bbc5e050c75397e482012810e2
radyvoniuk <radyvoniukkate@gmail.com>
2024-03-24 19:31:42 +0200

    adding code

---
+++ js/task-4.js
@@ -0,0 +1,51 @@
+const getTotalBalanceByGender = (users, gender) => {
+    const genderBalance = users.filter(user => user.gender === gender);
+    const balances = [];
+    for (const bal of genderBalance){
+        balances.push(bal.balance);
+    }
+    const totalBalance = balances.reduce((acc, balance) => acc + balance, 0);
+    return totalBalance;
+}
+
+const clients = [
+	{
+    name: "Moore Hensley",
+    gender: "male",
+    balance: 2811
+  },
+  {
+    name: "Sharlene Bush",
+    gender: "female",
+    balance: 3821
+  },
+  {
+    name: "Ross Vazquez",
+    gender: "male",
+    balance: 3793
+  },
+  {
+    name: "Elma Head",
+    gender: "female",
+    balance: 2278
+  },
+  {
+    name: "Carey Barr",
+    gender: "male",
+    balance: 3951
+  },
+  {
+    name: "Blackburn Dotson",
+    gender: "male",
+    balance: 1498
+  },
+  {
+    name: "Sheree Anthony",
+    gender: "female",
+    balance: 2764
+  }
+];
+
+console.log(getTotalBalanceByGender(clients, "male")); // 12053
+
+console.log(getTotalBalanceByGender(clients, "female")); // 8863
